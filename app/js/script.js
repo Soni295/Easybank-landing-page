f8e0c56c73b63736da9eee6f4122b77f4c485d3a
Sion Ramos de los Santos <sionandrieux@hotmail.com>
2021-02-02 11:07:57 -0300

    Presentacion y tarjetas
    Ordene de manera de que el diseno sea adaptable a tablets
    moviles y computadoras.
    Le agrege las tarjetas de descripcion mas icono principal

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,27 @@
 const btnHamburger = document.getElementById('btnHamburger')
 const header = document.getElementsByClassName('header')[0]
 const overlay = document.getElementsByClassName('overlay')[0]
+const fadeElements = [...document.getElementsByClassName('has-fade')]
+const body = document.getElementsByTagName('body')[0]
+
+
 
 btnHamburger.addEventListener('click', () => {
 
   if(header.classList.contains('open')) {
     header.classList.remove('open')
-
-    overlay.classList.remove('fade-in')
-    overlay.classList.add('fade-out')
+    body.classList.remove('noscroll')
+    fadeElements.forEach( element => {
+      element.classList.remove('fade-in')
+      element.classList.add('fade-out')
+    })
   }
   else {
     header.classList.add('open')
-
-    overlay.classList.remove('fade-out')
-    overlay.classList.add('fade-in')
+    body.classList.add('noscroll')
+    fadeElements.forEach( element => {
+      element.classList.remove('fade-out')
+      element.classList.add('fade-in')
+    })
   }
 })
